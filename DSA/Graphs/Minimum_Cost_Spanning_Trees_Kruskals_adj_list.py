3685aaba972e0ea549aa7f85a3f35e01b3c7e7cc
AmoghGOkade <161924221+AmoghGOkade@users.noreply.github.com>
2024-12-13 11:57:46 +0530

    Simplified Kruskal's a bit

--- DSA/Graphs/Minimum_Cost_Spanning_Trees_Kruskals_adj_list.py
+++ DSA/Graphs/Minimum_Cost_Spanning_Trees_Kruskals_adj_list.py
@@ -3,20 +3,20 @@ def kruskal(Wlist):
     component = {}
     tree_edges = []
     for u in Wlist.keys():
-        edges.extend([(d, u, v) for (v, d) in Wlist[u]])
-        '''l = []
+        '''edges.extend([(d, u, v) for (v, d) in Wlist[u]])'''
         for (v, d) in Wlist[u]:
-            l.append((d, u, v))
-        edges.extend(l)'''
+            edges.append((d, u, v))
+            
         component[u] = u
+        
     edges.sort()    # we have used the weight as the 1st index in edges, so it gets sorted based on that
 
     for (d, u, v) in edges:
         if component[u] != component[v]:    #edge doesn't create a cycle
             tree_edges.append((u, v))
-            c = component[u]        #can't write component[u] instead of c in the bottom line because component[u] might get changed before changing all nodes that have component[u]
+            c_old = component[u]        #can't write component[u] instead of c_old in the bottom line because component[u] might get changed before changing all nodes that have component[u]
             for i in Wlist.keys():
-                if component[i] == c:   #if a node is of component u, change it to component v
+                if component[i] == c_old:   #if a node is of component u, change it to component v
                     component[i] = component[v]
 
     return tree_edges
