7fc71da1d19ee78e8944c809839f380130b78c12
AmoghGOkade <161924221+AmoghGOkade@users.noreply.github.com>
2024-12-14 00:13:13 +0530

    Added union find code to Kruskal's

--- DSA/Graphs/Minimum_Cost_Spanning_Trees_Kruskals_adj_list.py
+++ DSA/Graphs/Minimum_Cost_Spanning_Trees_Kruskals_adj_list.py
@@ -20,3 +20,37 @@ def kruskal(Wlist):
                     component[i] = component[v]
 
     return tree_edges
+
+#Using union-find data structure (faster)
+def kruskal_fast(Wlist):
+    edges = []
+    mcst_edges = []
+    for u in Wlist.keys():
+        '''edges.extend([(d, u, v) for (v, d) in Wlist[u]])'''
+        for (v, d) in Wlist[u]:
+            edges.append((d, u, v))
+    
+    component = {}
+    members = {}  #key - component name, value - list of vertices belonging to that component
+    size = {}   #component name: no. of vertices belonging to it
+    for i in Wlist.keys():
+        component[i] = i
+        members[i] = [i]
+        size[i] = 1
+    
+    for (u, v, d) in edges:
+        if component[u] != component[v]:
+            mcst_edges.append((u, v, d))
+            if size[component[u]] > size[component[v]]:
+                c_old = component[v]
+                c_new = component[u]
+            else:
+                c_old = component[u]
+                c_new = component[v]
+            
+            for i in members[c_old]:
+                component[i] = c_new
+                members[c_new].append(i)    #need not destroy c_old data of members and size, because it is never being used
+                size[c_new]+=1
+
+    return mcst_edges
