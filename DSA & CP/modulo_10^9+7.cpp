6011a882597ae409a54960d3379104c8f29e9651
AmoghGOkade <161924221+AmoghGOkade@users.noreply.github.com>
2026-01-07 18:09:00 +0530

    Update modulo_10^9+7.cpp

--- DSA & CP/modulo_10^9+7.cpp
+++ DSA & CP/modulo_10^9+7.cpp
@@ -1,6 +1,9 @@
 //Any operation relating the modulo only comes when computing the answer (that you need to return as modulo 10^9 + 7)
 //The key is to just add a %M smartly wherever any such computation happens
 
+//In some cases, long long ints can handle whatever values you are expected to use %M for. (Leetcode 1339. Maximum product of splitted binary tree)
+//In such cases, computation can just be done using long long ints and the final return statement can have a %M.
+
 #define M 1000000007
 
 ( a + b) % M = ( ( a % M ) + ( b % M ) ) % M;
