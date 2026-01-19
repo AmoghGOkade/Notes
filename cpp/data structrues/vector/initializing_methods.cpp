a8938d38a11afc1e6b0b56d61783c6182f012225
AmoghGOkade <161924221+AmoghGOkade@users.noreply.github.com>
2026-01-19 22:34:58 +0530

    Create initializing_methods.cpp

---
+++ cpp/data structrues/vector/initializing_methods.cpp
@@ -0,0 +1,13 @@
+M1
+vector<int> v;
+v.push_back(1); v.push_back(1); v.push_back(1);
+
+M2
+vector<int> v = {1, 1, 1};
+
+M3
+int n = 3;
+vector<int> v(n, 1);    //Gives a vector having 3 1s
+
+Example for 2D vector:-
+vector<vector<int>> v(m, vector<int>(n, 0));    //m x n matrix initialized with 0s
