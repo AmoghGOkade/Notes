8a299a977f2628057cdf45927bdf13b764db5782
AmoghGOkade <161924221+AmoghGOkade@users.noreply.github.com>
2026-02-21 15:20:53 +0530

    Update initializing_methods.cpp

--- cpp/data structrues/vector/initializing_methods.cpp
+++ cpp/data structrues/vector/initializing_methods.cpp
@@ -5,7 +5,7 @@ v.push_back(1); v.push_back(1); v.push_back(1);
 M2
 vector<int> v = {1, 1, 1};
 
-M3
+M3    //doesn't work for global vectors
 int n = 3;
 vector<int> v(n, 1);    //Gives a vector having 3 1s
 
